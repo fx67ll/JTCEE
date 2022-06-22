e1c55586a2e67953b981a73793a4d09dc267015d
fx67ll <2717745014@qq.com>
2022-06-23 00:44:50 +0800

    add VueI18n

--- App.vue
+++ App.vue
@@ -1,17 +1,24 @@
 <script>
-	export default {
-		onLaunch: function() {
-			// console.log('App Launch')
-		},
-		onShow: function() {
-			// console.log('App Show')
-		},
-		onHide: function() {
-			// console.log('App Hide')
-		}
+export default {
+	onLaunch: function() {
+		// console.log('App Launch')
+		uni.setLocale('zh-Hans');
+		// this.$i18n.locale = 'zh-Hans';
+	},
+	onShow: function() {
+		// console.log('App Show')
+	},
+	onHide: function() {
+		// console.log('App Hide')
 	}
+};
 </script>
 
 <style>
-	/*每个页面公共css */
+/*这里是每个页面公共css */
+
+/* picker按钮颜色公共样式 */
+.uni-picker-container .uni-picker-action.uni-picker-action-confirm {
+	color: #5bc797 !important;
+}
 </style>
---
+++ locale/index.js
@@ -0,0 +1,10 @@
+import en from './l18n/en.json'
+import zhHans from './l18n/zh-Hans.json'
+import zhHant from './l18n/zh-Hant.json'
+import ja from './l18n/ja.json'
+export default {
+	en,
+	'zh-Hans': zhHans,
+	'zh-Hant': zhHant,
+	ja
+}
