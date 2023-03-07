ff86908bc878b73618de78f1603ad1cfce8f9c8c
fx67ll <2717745014@qq.com>
2023-03-07 11:04:32 +0800

    fix

--- locale/index.js
+++ locale/index.js
@@ -1,5 +1,5 @@
-import zhHans from './l18n/zh-Hans.json'
-import ja from './l18n/ja.json'
+import zhHans from './i18n/zh-Hans.json'
+import ja from './i18n/ja.json'
 export default {
 	'zh-Hans': zhHans,
 	ja
